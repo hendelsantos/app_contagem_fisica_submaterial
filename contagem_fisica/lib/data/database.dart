b4c54b3fdf8edab9c09953cc43bf9b6e355df4e1
hendelsantos <hendelsantos@gmail.com>
2026-08-11 21:54:00 -0300

    Corrigir unidade dos materiais da Axalta: Kg -> L (litros)
    - seed.dart: 15 materiais Axalta (Primer, Basecoat, Clearcoat, Solvente)
      alterados de 'Kg' para 'L'
    - database.dart: schema v1 -> v2 com onUpgrade que atualiza
      unidade='L' WHERE fornecedor='Axalta' em instalacoes existentes

--- contagem_fisica/lib/data/database.dart
+++ contagem_fisica/lib/data/database.dart
@@ -24,13 +24,20 @@ class AppDatabase extends _$AppDatabase {
   AppDatabase.forTesting(super.e);
 
   @override
-  int get schemaVersion => 1;
+  int get schemaVersion => 2;
 
   @override
   MigrationStrategy get migration => MigrationStrategy(
         onCreate: (m) async {
           await m.createAll();
           await _semear();
+        },
+        onUpgrade: (m, from, to) async {
+          if (from < 2) {
+            await customStatement(
+              "UPDATE materiais SET unidade = 'L' WHERE fornecedor = 'Axalta'",
+            );
+          }
         },
         beforeOpen: (details) async {
           if (details.wasCreated) return;
